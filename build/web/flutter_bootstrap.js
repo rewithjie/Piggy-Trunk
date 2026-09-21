3646242a7423c4d8cd34c06e2c7e40545e1dc844
rewithjie <rewithjie@gmail.com>
2026-09-22 01:26:17 +0800

    Optimize iOS Safari PWA meta tags

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "114540657" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2731560135" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
